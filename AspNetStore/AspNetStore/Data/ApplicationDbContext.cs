568971ad7a080d65b82a34f142e9e919187c4cc6
andriisytnyk <sytnyk.andrii@gmail.com>
2021-12-04 00:32:42 +0200

    Category model was created. DbContext was created. Dependence of the DbContext was injected. Database and first migration were created.

---
+++ AspNetStore/AspNetStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
