9e8086138033dbf5e77f464e3362f70bacd4d23e
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2021-12-03 22:42:56 +0200

    Delete AspNetStore directory

--- AspNetStore/AspNetStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
