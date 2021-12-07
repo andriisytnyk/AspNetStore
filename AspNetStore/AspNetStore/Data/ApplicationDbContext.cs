155db7413df51eda7a8465ef2522757c71822d37
andriisytnyk <sytnyk.andrii@gmail.com>
2021-12-07 13:03:25 +0200

    Application type model was created. ApplicationTypeController was created. Migration for creating application type table was existed. Views Index and Create for Category were created.

--- AspNetStore/AspNetStore/Data/ApplicationDbContext.cs
+++ AspNetStore/AspNetStore/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace AspNetStore.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
