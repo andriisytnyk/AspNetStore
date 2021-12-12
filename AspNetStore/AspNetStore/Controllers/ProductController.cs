cdda84e6824dc769418cdbda1718b4f044359b17
andriisytnyk <sytnyk.andrii@gmail.com>
2021-12-12 19:20:23 +0200

    Product model was created. Product table in db was created. ProductController was created. Views for product were created. Sweetalert was added in Upsert.cshtml.

---
+++ AspNetStore/AspNetStore/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using AspNetStore.Data;
+using AspNetStore.Models;
+using AspNetStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(c => c.Id == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(c => new SelectListItem
+            //{
+            //    Text = c.Name,
+            //    Value = c.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //Product product = new Product();
+
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productVM);
+            }
+        }
+
+        //POST - UPSERT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Product.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Product.Find(id);
+            if (obj == null) 
+            { 
+                return NotFound(); 
+            }
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Product.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Product.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AspNetStore/AspNetStore/Data/ApplicationDbContext.cs
+++ AspNetStore/AspNetStore/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace AspNetStore.Data
     {
         public DbSet<Category> Category { get; set; }
         public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
