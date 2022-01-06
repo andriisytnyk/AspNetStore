5766bad3b4d82e4769affe3dd1417954dc556b5d
andriisytnyk <sytnyk.andrii@gmail.com>
2022-01-06 21:28:45 +0200

    Model, View and Controller for Home page were created.

--- AspNetStore/AspNetStore/Controllers/HomeController.cs
+++ AspNetStore/AspNetStore/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using AspNetStore.Models;
+﻿using AspNetStore.Data;
+using AspNetStore.Models;
+using AspNetStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +15,22 @@ namespace AspNetStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
