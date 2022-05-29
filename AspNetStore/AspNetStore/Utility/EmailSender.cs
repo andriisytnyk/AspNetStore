3a271ebc90e231387374520efbc1acc91ed3c2fe
andriisytnyk <smegol55@gmail.com>
2022-05-29 12:06:51 +0300

    Email sending add.

---
+++ AspNetStore/AspNetStore/Utility/EmailSender.cs
@@ -0,0 +1,73 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetStore.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public MailJetSettings MailJetSettings { get; set; }
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailJetSettings = _configuration.GetSection("MailJet").Get<MailJetSettings>();
+
+            MailjetClient client = new MailjetClient(MailJetSettings.ApiKey, MailJetSettings.SecretKey)
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }.Property(Send.Messages, new JArray 
+            {
+                new JObject 
+                {
+                    {
+                        "From",
+                        new JObject 
+                        {
+                            {"Email", "smegol55@gmail.com"},
+                            {"Name", "Andrii"}
+                        }
+                    }, 
+                    {
+                        "To",
+                        new JArray 
+                        {
+                            new JObject 
+                            {
+                                {"Email", email}, 
+                                {"Name", "Andrii"}
+                            }
+                        }
+                    }, 
+                    {
+                        "Subject",
+                        subject
+                    }, 
+                    {
+                        "HTMLPart",
+                        body
+                    }
+                }
+            });
+            await client.PostAsync(request);
+        }
+    }
+}
