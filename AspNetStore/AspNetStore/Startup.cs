ced0aee2fc952a42f938fa73d40719b3134fadef
andriisytnyk <smegol55@gmail.com>
2022-05-29 12:08:23 +0300

    Service settings change.

--- AspNetStore/AspNetStore/Startup.cs
+++ AspNetStore/AspNetStore/Startup.cs
@@ -1,16 +1,14 @@
 using AspNetStore.Data;
+using AspNetStore.Utility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetStore
 {
@@ -42,6 +40,7 @@ namespace AspNetStore
                 .AddDefaultTokenProviders()
                 .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddTransient<IEmailSender, EmailSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
