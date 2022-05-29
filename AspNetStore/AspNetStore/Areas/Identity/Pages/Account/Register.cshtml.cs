1a9004a18b56243d83adf563a6573d809bd5a883
andriisytnyk <smegol55@gmail.com>
2022-05-29 12:05:28 +0300

    Registration for new admin user change.

--- AspNetStore/AspNetStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AspNetStore/AspNetStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -116,7 +116,10 @@ namespace AspNetStore.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (!User.IsInRole(WC.AdminRole))
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        else
+                            return RedirectToAction("Index");
                         return LocalRedirect(returnUrl);
                     }
                 }
